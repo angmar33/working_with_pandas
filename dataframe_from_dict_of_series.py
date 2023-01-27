4cc1eeb375e761fda8f992292aabfe4e32636fd7
angmar33 <paultsvilyuk@gmail.com>
2023-01-27 08:03:46 -0800

    dataframe creation with dicts.

--- dataframe_from_dict_of_series.py
+++ dataframe_from_dict_of_series.py
@@ -16,3 +16,40 @@ s = {
 df = pd.DataFrame(s)
 print("dataframe created from dict of series with index")
 print(df)
+
+
+"""
+dataframe created from dict of series
+   a  b
+0  1  2
+1  2  3
+--------------------
+dataframe created from dict of series with index
+     a    b
+a  1.0  NaN
+b  2.0  2.0
+c  NaN  3.0
+"""
+
+
+d = {"a": [1, 2, 3, 4], "b": [2, 4, 6, 8]}
+df = pd.DataFrame.from_dict(d)
+print("dataframe created from from_dict")
+print(df)
+print("--------------------")
+
+df = pd.DataFrame.from_dict(d, orient="index")
+print("dataframe created from from_dict and set the orient")
+print(df)
+"""
+dataframe created from from_dict
+0  1  2
+1  2  4
+2  3  6
+3  4  8
+--------------------
+dataframe created from from_dict and set the orient
+   0  1  2  3
+b  2  4  6  8
+a  1  2  3  4
+"""
